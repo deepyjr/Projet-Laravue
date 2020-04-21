2a9e95792729ba17597ea8623316526d9d0edac9
Florian GILLET pc fixe <florian.gillet@estiam.com>
2020-04-21 15:55:28 +0200

    ajout du dashbord controller

---
+++ app/Http/Controllers/dashbordController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class dashbordController extends Controller
+{
+    /**
+     * 
+     */
+    public function getTodayNbWash($clientId)
+    {
+    }
+}
